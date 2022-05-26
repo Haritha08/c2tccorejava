197ae8f85130d16ee4758ef178eed29acb5084d3
harit <harit@192.168.100.7>
2022-05-26 21:34:19 +0530

    First commit

--- B12-Day1 java intro/src/HelloB12/java/Arithmetic_Operator.java
+++ B12-Day1 java intro/src/HelloB12/java/Arithmetic_Operator.java
@@ -3,8 +3,15 @@ package HelloB12.java;
 public class Arithmetic_Operator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	//int a=10*2+3-4;
+		//int a=1*6/4;
+		//int a=2+6;
+		//int a=2*6;
+		//int a=2-6;
+		//int a=6/2;
+		int a=6%2;
+		
+    System.out.println(a);
 	}
 
 }
---
+++ B12-Day1 java intro/src/HelloB12/java/DemoCon.java
@@ -0,0 +1,13 @@
+package HelloB12.java;
+
+public class DemoCon {
+	int num;
+
+	public static void main(String[] args) {
+		DemoCons obj=new DemoCons();
+		System.out.println("The value of num"+obj.num);
+	
+
+	}
+
+}
