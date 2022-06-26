acc045c3794b0cab4e2cd19aa3f30e018ef2518a
harit <harit@LAPTOP-4N89LG76>
2022-06-26 16:00:02 +0530

    First Commit

---
+++ B12-Day1 java intro/src/com/tnsif/exception/Demotrycatch.java
@@ -0,0 +1,33 @@
+package com.tnsif.exception;
+
+public class Demotrycatch extends Exception{
+
+	public static void main(String[] args) {
+		 int n1=50/2;
+		System.out.println(n1);
+		
+
+		int[] arr=new int[5];
+	
+		try
+		{
+		
+			int a[]=new int [5];
+			a[8]= 30/4;
+		
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println(e);
+		} 
+		catch(ArithmeticException e)
+		{
+			System.out.println(e);
+		}
+		finally
+		{
+			System.out.println("end of block");
+		}
+	}
+
+}
