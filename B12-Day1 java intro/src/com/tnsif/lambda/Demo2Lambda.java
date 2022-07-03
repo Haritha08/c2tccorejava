be60e5c6ddfdbbf699869b17ea29e5d77a99a2e3
harit <harit@LAPTOP-4N89LG76>
2022-07-03 13:59:04 +0530

    First Commit

--- B12-Day1 java intro/src/com/tnsif/lambda/Demo2Lambda.java
+++ B12-Day1 java intro/src/com/tnsif/lambda/Demo2Lambda.java
@@ -1,9 +1,23 @@
 package com.tnsif.lambda;
 
+@FunctionalInterface
+interface shape
+{
+	void draw();
+}
+
 public class Demo2Lambda {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		shape circle=()->System.out.println("This is a draw method for circle");
+		shape rect=()->System.out.println("This is a draw method for rectangle ");
+		shape triangle=()->System.out.println("This is a draw method for triangle ");
+
+
+		circle.draw();
+		rect.draw();
+		triangle.draw();
 
 	}
 
